be1b82a7e6366700919dcac64bbe65cf6688ba69
TShambetaliev <tshambetaliev@gmail.com>
2022-12-08 22:06:46 +0600

    foreach

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 import java.util.Arrays;
 public class Main {
+    public static void bubbleSort(double[] sortArr){
+        for (int i = 0; i < sortArr.length - 1; i++) {
+            for(int j = 0; j < sortArr.length - i - 1; j++) {
+                if(sortArr[j + 1] < sortArr[j]) {
+                    double swap = sortArr[j];
+                    sortArr[j] = sortArr[j + 1];
+                    sortArr[j + 1] = swap;
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         double[] numbers = {1.5, -3, 5, 7, 8, 11, 13, 6, 20, 13, 95, 63, 4, 5.5, 15};
         System.out.println(Arrays.toString(numbers));
@@ -15,6 +26,12 @@ public class Main {
                 System.out.println(elem);
             }
         }
-        System.out.println("среднее арифметическое число равен" + sum / kol)
+        System.out.println("среднее арифметическое число равен" + sum / kol);
+        System.out.println("-----------" );
+        double[] sortArr = {1.5, -3, 5, 7, 8, 11, 13, 6, 20, 13, 95, 63, 4, 5.5, 15};
+        bubbleSort(sortArr);
+        for(int i = 0; i < sortArr.length; i++){
+            System.out.print(sortArr[i] + "\n");
+        }
     }
     }
