bbb0dd584479dee06dbe76088fe88b1f322ebd79
TShambetaliev <tshambetaliev@gmail.com>
2022-12-08 21:49:37 +0600

    foreach

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
 import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         double[] numbers = {1.5, -3, 5, 7, 8, 11, 13, 6, 20, 13, 95, 63, 4, 5.5, 15};
-
-
         System.out.println(Arrays.toString(numbers));
         double sum = 0.0;
         int kol = 0;
         boolean ech = false;
-
         for (double elem : numbers) {
-
             if (elem < 0) {
                 ech = true;
             } else if (elem > 0 && ech) {
@@ -20,21 +15,6 @@ public class Main {
                 System.out.println(elem);
             }
         }
-
-        System.out.println("среднее арифметическое число равен" + sum / kol);
-        double[] number = {1.5, -3, 5, 7, 8, 11, 13, 6, 20, 13, 95, 63, 4, 5.5, 15};
-
-        boolean sort = false;
-        while (!sort) {
-            sort = true;
-            for (int i = 0; i < number.length; i++) {
-                if (number[i] < number[i - 1]) {
-                    double temp = number[i];
-                    number[i - 1] = temp;
-                    sort = false;
-                }
-            }
-            System.out.println(number);
-        }
+        System.out.println("среднее арифметическое число равен" + sum / kol)
     }
-}
+    }
