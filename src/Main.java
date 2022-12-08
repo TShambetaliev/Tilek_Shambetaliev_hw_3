65a7bc4a78fe86f315b2570fe897073070677d55
TShambetaliev <tshambetaliev@gmail.com>
2022-12-08 21:02:50 +0600

    foreach

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,39 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        double[] numbers = new double[15];
-        for (int i = 0; i < numbers.length; i += 1.5) {
-            numbers[i] = i + (-4.5);}
+        double[] numbers = {1.5, -3, 5, 7, 8, 11, 13, 6, 20, 13, 95, 63, 4, 5.5, 15};
 
-            System.out.println(Arrays.toString(numbers));
-            double sum = 0.0;
-            int kol = 0;
 
-            for (int i = 0; i < numbers.length; i++) {
-                if (numbers[i] < 0) {
-                    for (int j = i + 1; j < numbers.length; j++) {
-                        if (numbers[j] > 0) {
-                            sum = sum + numbers[j];
+        System.out.println(Arrays.toString(numbers));
+        double sum = 0.0;
+        int kol = 0;
+        boolean ech = false;
 
-                            kol++;
-                        }
-                    }
-                    break;
+        for (double elem : numbers) {
+
+            if (elem < 0) {
+                ech = true;
+            } else if (elem > 0 && ech) {
+                sum = sum + elem;
+                kol++;
+                System.out.println(elem);
+            }
+        }
+
+        System.out.println("среднее арифметическое число равен" + sum / kol);
+        double[] number = {1.5, -3, 5, 7, 8, 11, 13, 6, 20, 13, 95, 63, 4, 5.5, 15};
+
+        boolean sort = false;
+        while (!sort) {
+            sort = true;
+            for (int i = 0; i < number.length; i++) {
+                if (number[i] < number[i - 1]) {
+                    double temp = number[i];
+                    number[i - 1] = temp;
+                    sort = false;
                 }
             }
-            System.out.println("sum"+sum);
-            System.out.println("kolichestvo"+kol);
-            System.out.println("sum/kolichestvo"+sum/kol;
+            System.out.println(number);
         }
     }
+}
